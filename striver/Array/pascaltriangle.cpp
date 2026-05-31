1a30ee27cc9f546eda6901023060ca86ba2e1ac8
Raushan Raj bit <raushanraaj98718@gmail.com>
2026-05-31 11:51:20 +0530

    pascaltrianglr

---
+++ striver/Array/pascaltriangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+
+        for (int row = 1; row <= numRows; row++) {
+            vector<int> currRow;
+            long long val = 1;
+
+            currRow.push_back(1);
+
+            for (int col = 1; col < row; col++) {
+                val = val * (row - col);
+                val = val / col;
+                currRow.push_back(val);
+            }
+
+            ans.push_back(currRow);
+        }
+
+        return ans;
+    }
+};
