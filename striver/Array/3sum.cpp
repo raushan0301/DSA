c39132202ff64b4820a6fb177ad03452698ee8ab
Raushan Raj bit <raushanraaj98718@gmail.com>
2026-06-03 11:01:30 +0530

    3sum2pointers

--- striver/Array/3sum.cpp
+++ striver/Array/3sum.cpp
@@ -4,7 +4,7 @@ public:
         set<vector<int>> st;
         int n = nums.size();
         for(int i=0; i<n; i++){
-            set<int> hashset;
+            unordered_set<int> hashset;
             for(int j=i+1; j<n; j++){
                 int third = -(nums[i]+ nums[j]);
                 if(hashset.find(third) != hashset.end()){
@@ -25,3 +25,42 @@ public:
 
 
 // better soln but not best tc o(n^2 * log(m))
+
+
+
+
+
+
+// optimal 2 pointer logic.  tc= O(nlogn) + O(n^2)
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0 ; i<n ; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j= i+1;
+            int k= n-1;
+
+        while(j<k){
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum<0){
+                j++;
+            }
+            else if (sum >0){
+                k--;
+            }
+            else{
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while(j<k && nums[j] == nums[j-1]) j++;
+                while(j<k && nums[k] == nums[k+1]) k--;            
+           }
+          }  
+        }return ans;
+    }
+};
