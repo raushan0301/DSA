24980a13289a8e8fd04112abf465c44901c8c291
Raushan Raj bit <raushanraaj98718@gmail.com>
2026-06-16 16:07:13 +0530

    comment edit

--- Linkedlist/deletenode.cpp
+++ Linkedlist/deletenode.cpp
@@ -1,11 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
