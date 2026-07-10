d6adae2fee6844022d30fff146bf1000420407a8
Raushan Raj bit <raushanraaj98718@gmail.com>
2026-07-10 18:49:51 +0530

    stack

---
+++ Stackqueue/validprantensis.cpp
@@ -0,0 +1,52 @@
+class MinStack {
+private:
+  stack<long long> st;
+  long long mini;
+
+public:
+  MinStack() {}
+
+  void push(int value) {
+
+    if (st.empty()) {
+      mini = value;
+      st.push(value);
+      return;
+    }
+
+    if (value >= mini) {
+      st.push(value);
+    } else {
+      st.push(2LL * value - mini);
+      mini = value;
+    }
+  }
+
+  void pop() {
+
+    if (st.empty())
+      return;
+
+    long long x = st.top();
+    st.pop();
+
+    if (x < mini) {
+      mini = 2LL * mini - x;
+    }
+  }
+
+  int top() {
+
+    if (st.empty())
+      return -1;
+
+    long long x = st.top();
+
+    if (x >= mini)
+      return (int)x;
+
+    return (int)mini;
+  }
+
+  int getMin() { return (int)mini; }
+};
