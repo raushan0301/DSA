1588590feb32ade73bbf0dbd0dd161106cfda2c7
Raushan Raj bit <raushanraaj98718@gmail.com>
2026-07-10 10:24:26 +0530

    bit mani

---
+++ Bitmanupulation/Dividetwointegers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == divisor)
+        return 1;
+        bool sign = true;
+        if(dividend >= 0 && divisor < 0 ) sign = false ;
+        if(dividend <= 0 && divisor > 0 ) sign = false ;
+        long long n = abs((long long)dividend);
+        long long d = abs((long long)divisor);
+ 
+        long long quotient = 0;
+        while (n>= d){
+            int cnt = 0;
+            while (n >= (d << (cnt +1))){
+            cnt +=1;
+        }
+        quotient += 1 <<cnt;
+        n -= (d <<cnt);
+        }
+
+    if (quotient == (1 << 31) && sign ){
+        return INT_MAX;
+    }
+    if (quotient == (1 << 31) && sign ){
+        return INT_MIN;
+    }
+
+    return sign ? quotient : -quotient;
+    }
+};
